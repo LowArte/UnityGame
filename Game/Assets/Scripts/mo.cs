ddfbfbe66cfc31a59d9e52b4fc0d413d0460dd2b
DontBlowMyMind <47981997+DontBlowMyMind@users.noreply.github.com>
2019-02-27 16:35:18 +0300

    fullmove
    fmove

--- Game/Assets/Scripts/mo.cs
+++ Game/Assets/Scripts/mo.cs
@@ -5,28 +5,35 @@ using UnityEngine.AI;
 
 public class mo : MonoBehaviour
 {
+    public float speed;
+    Animator anim;
     NavMeshAgent nav;
+    
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
      void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
+
+            anim.SetBool("Walk", true);
             if (Physics.Raycast(ray, out hit, 5000))
             {
                 nav.SetDestination(hit.point);
-                nav.Resume();
-            }
-            if (Vector3.Distance(transform.position,nav.destination)<0.1f)
-            {
-                nav.Stop();
+                nav.isStopped=false;   
             }
         }
-        
+
+        if (Vector3.Distance(transform.position, nav.destination) <= 0.1f)
+        {
+            anim.SetBool("Walk", false);
+            nav.isStopped = true; ;
+        }
     }
     
     
