d4168c8dafd2dc312d93083bf32ee0c4185dd4a4
DontBlowMyMind <47981997+DontBlowMyMind@users.noreply.github.com>
2019-02-27 14:17:11 +0300

    movemose
    movemose

---
+++ Game/Assets/Scripts/mo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class mo : MonoBehaviour
+{
+    NavMeshAgent nav;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+     void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 5000))
+            {
+                nav.SetDestination(hit.point);
+                nav.Resume();
+            }
+            if (Vector3.Distance(transform.position,nav.destination)<0.1f)
+            {
+                nav.Stop();
+            }
+        }
+        
+    }
+    
+    
+}
