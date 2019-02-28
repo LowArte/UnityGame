de71a3e1154edc8dcceafdf084fbf8c144cf4752
DontBlowMyMind <47981997+DontBlowMyMind@users.noreply.github.com>
2019-02-28 16:10:47 +0300

    aicontrol

--- Game/Assets/Ai/AiScripts/AiControls.cs
+++ Game/Assets/Ai/AiScripts/AiControls.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AiControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AiControl move;
+    
     void Start()
     {
-        
+        move = GetComponent<AiControl>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        move.Momuve();
     }
+
+  
 }
